8b0cce609f532a3e24d7118eae04e6ddf38b5297
Zubov <Vlad>
2020-06-24 22:54:11 +0200

    Route agenda modif

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import Agenda from './components/Agenda.jsx';
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
-import Agenda from "./components/Agenda"
 
 const App = () => {
   return (
@@ -11,7 +10,7 @@ const App = () => {
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/Agenda" component={Agenda} />
+        <Route path="/agenda" component={Agenda} />
       </Switch>
     </div>
   );
