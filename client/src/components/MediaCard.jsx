3779d30b5c30960456426298ae247c815176f5d8
Guillaume-Kreit <Guillaume.kreit@outllook.com>
2020-06-25 00:13:31 +0200

    props me

--- client/src/components/MediaCard.jsx
+++ client/src/components/MediaCard.jsx
@@ -16,11 +16,13 @@ const useStyles = makeStyles({
     height: 240,
   },
   button: {
-    background: "yellow",
+    background: "#f00",
+    margin: 'auto',
+    marginBottom: 20,
   }
 });
 
-export default function MediaCard() {
+const MediaCard = ({ choice, image, description }) => {
   const classes = useStyles();
 
   return (
@@ -28,15 +30,15 @@ export default function MediaCard() {
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image="https://s3-alpha-sig.figma.com/img/0082/51f6/5877dcffd84643448a1b0fb88d4a99a8?Expires=1593993600&Signature=TLXvg~qecv-Q56jZw9DHQ2j5z6fh0KH4Z04LxTpu7-eCaNsx9sWwbqXEDAZWse2tucxybk7WrOVpjG8QvDrHjiIpQpc4hbKCqDmaunjKI~3P4q5ocjKO~Q21asWufnCxXeaMDT5n2rMWgazXkFr-AN4iCcwlSrt1eiiFtbIXRkoBtMwWzyw98KDMP50SMWlqPGrSQN71kp-IvXZCHxdIbbaewrjTDhN6LRIZ5Fe0tNFIGEQk2-32kzLixtwtm23EowBwlV6odGevlkQ-SA7u1BKG6aXcFYsZ-kz6ny9h0DpqgHlvc2ioJSjb6CCa0dq22cSR5tM8gIodnt5gUqfVBg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+          image={image}
           title="Contemplative Reptile"
         />
         <CardContent>
         <Button size="small" variant="contained" className={classes.button}>
-          I took a risk
+          {choice}
         </Button>
           <Typography variant="body2" color="textSecondary" component="p">
-          You took a risk and you’re wondering if you got HIV. Don’t panic and tell us everything
+          {description}
           </Typography>
         </CardContent>
       </CardActionArea>
@@ -44,5 +46,4 @@ export default function MediaCard() {
   );
 }
 
-
-
+export default MediaCard;
