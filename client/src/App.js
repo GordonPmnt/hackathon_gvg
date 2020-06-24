8879b27df70706ee2b801fcc6f18cf961a30530f
Guillaume-Kreit <Guillaume.kreit@outllook.com>
2020-06-24 22:31:48 +0200

    component card

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import Agenda from './components/Agenda.jsx'
+import MediaCard from './components/card'
 
 const App = () => {
   return (
     <div>
-      <Agenda />
+      <MediaCard/>
+      {/* 
+      <MediaCard/>
+      <MediaCard/>
+      <MediaCard/> 
+      */}
+
     </div>
   );
 }
