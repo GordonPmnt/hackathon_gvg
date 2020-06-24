92de223706295aeead3af614bcd544eaec3a5504
Gordon Preumont <preumontgr@gmail.com>
2020-06-24 22:19:08 +0200

    agenda basics

---
+++ client/src/components/Ticket.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    padding: 5%;
+    margin: 5%;
+    width: 100%;
+    height: 50px;
+    border: solid 1px black;
+`
+
+const Ticket = () => {
+    return (
+        <Container>
+            POUET
+        </Container>
+    )
+}
+
+export default Ticket;
