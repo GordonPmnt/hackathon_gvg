d5b49a8f862ac04055182c06251c31721af8787e
Gordon Preumont <preumontgr@gmail.com>
2020-06-25 11:52:01 +0200

    fix Writting

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import MediaCard from './MediaCard.jsx';
-import Writing from './writing.jsx';
+import Writing from './Writing.jsx';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
