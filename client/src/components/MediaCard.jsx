27f202943585ac975fd5eb3ad1bad53808c83095
Gordon Preumont <preumontgr@gmail.com>
2020-06-25 07:42:16 +0200

    modals basics

--- client/src/components/MediaCard.jsx
+++ client/src/components/MediaCard.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
+import ModalTreatment from './ModalTreatment';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
@@ -15,11 +16,6 @@ const useStyles = makeStyles({
   media: {
     height: 240,
   },
-  button: {
-    background: "#f00",
-    margin: 'auto',
-    marginBottom: 20,
-  }
 });
 
 const MediaCard = ({ choice, image, description }) => {
@@ -34,9 +30,9 @@ const MediaCard = ({ choice, image, description }) => {
           title="Contemplative Reptile"
         />
         <CardContent>
-        <Button size="small" variant="contained" className={classes.button}>
-          {choice}
-        </Button>
+        <ModalTreatment 
+          choice={choice}
+        />
           <Typography variant="body2" color="textSecondary" component="p">
           {description}
           </Typography>
